0521a4ce1ac4a875d77e2c9097078d6f420f55a7
Nixie <havenga24@gmail.com>
2023-06-04 00:47:53 -0400

    Update Discord_OpenAIBot.py
    Added regions and comments. Updated chat and reset commands to global vars

--- Discord_OpenAIBot.py
+++ Discord_OpenAIBot.py
@@ -2,6 +2,8 @@ import os
 import discord
 import openai
 
+#region Global Vars
+
 #Log program and bot response types to a logfile, does not write conversation data to log.
 logging = False
 
@@ -10,6 +12,9 @@ discord_token = 'YOUR_BOT_KEY'
 bot_intents = discord.Intents.default()
 bot_intents.message_content = True
 Discord_Client = discord.Client(intents=bot_intents)
+
+chat_command = "/c" #Defines what a message needs to start with for ai to respond to a message.
+reset_command = "/reset" #Defines the reset command for the conversation history of the bot.
 
 #OpenAI API compleation Variables and API key, again in code bc lazy
 openai.api_key = ("YOUR_OPENAI_API_KEY")
@@ -20,7 +25,9 @@ top_prob = .9 #Defines the probability mass used for text generation, refer to o
 freq_penailty = 1.8 #Defines the penailty for reused tokens, refer to openai docs.
 bias = {"21017": -100} #Defines tokens that should not be generated, currently the tokens for '###' have a 100% chance of NEVER appaering. Refer to openai docs for more info
 
-####Functions for Chat History Management####
+#endregion
+
+#region Conversation Management
 
 #Get conversation file for the discord server bot is called from or create an empty file for new servers.
 def GetConversation(server_id):
@@ -62,7 +69,9 @@ def OpenaiApiRequest(api_prompt):
     response = response['choices'][0]['text']
     return(response)
 
-####Discord Client and Chat Logic####
+#endregion
+
+#region Discord Events
 
 #Startup event
 @Discord_Client.event
@@ -72,16 +81,20 @@ async def on_ready():
 #Bot response event
 @Discord_Client.event
 async def on_message(message):
-    if message.content.startswith('/c').lower():
+    if message.content.startswith(chat_command):
         server_id = message.guild.id
-        formatting = " \n\n###\n\n"
+        formatting = " \n\n###\n\n" # Seporator used to seperate each message.
 
         conversation = GetConversation(server_id)
-        user_message = message.content
-        user_message = user_message[2:] # Removes /c from message
+        user_message = message.content #Content of message send on discord server
+        user_message = user_message[2:] # Removes chat command from message
 
-        prompt = conversation + user_message + formatting
+        prompt = conversation + user_message + formatting #Conversation must be added to begining of prompt. fomatting added to the end to prep for next message.
         failed = False
+        #This whole try expect block attempts to get a response from the model, it trys a second time when the api throws an error, most likely max tokens
+        #It tries twice, editing the conversation to the last 10 messages on the second attempt to try and reduce token usage
+        #If that fails it wipes the conversation, warning the user and setting failed to True, saving the empty conversation to the chat history file.
+        #Otherwise it will just update the chat history file with the formatting required to keep the chat history concise.
         try:
             response = OpenaiApiRequest(prompt)
         except:
@@ -97,9 +110,11 @@ async def on_message(message):
             SaveConversation(server_id, conversation)
         else:
             SaveConversation(server_id, conversation + user_message + formatting + response + stop_generation + formatting)
+        #Discord won't let you send a message with nothing in it, models sometimes will respond with an empty string, this checks for this and warns the user.
         if response == "" or response == " ":
             response = "Attempted to send empty message"
             await message.channel.send(response)
+        #This is checking to make sure the response doesn't hit discord's max character limit of 2000, with a double check for part 2 of the response.
         elif len(response) > 2000:
             length = len(response)
             part1 = response[:2000]
@@ -116,9 +131,13 @@ async def on_message(message):
                 await message.channel.send(part2)
         else:
             await message.channel.send(response)
-    if message.content.startswith("/reset"):
+    #Resets the conversation file apon a reset commmand
+    if message.content.startswith(reset_command):
         GetConversation(server_id)
         ClearConversation(server_id)
         alert = "Conversation reset"
         await message.channel.send(alert)
+
+#endregion
+
 Discord_Client.run(discord_token)
